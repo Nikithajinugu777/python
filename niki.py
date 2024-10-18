a419d86e03bb13184c93df19846b1deb84990817
Nikithajinugu777 <23pa1a0593@vishnu.edu.in>
2024-10-18 11:38:22 +0530

    Add files via upload

--- niki.py
+++ niki.py
@@ -1,17 +1,4 @@
-def add(a,b):
- print(a+b)
-add(8,4)
-def sub(a,b):
- print(a-b)
-sub(8,4)
-def multi(a,b):
- print(a*b)
-multi(8,4)
-def div(a,b):
- print(a/b)
-div(8,4)
-#output
-12
-4
-32
-2.0
+f=open("new.txt",'w')
+f.write("this is my first file program")
+f.close()
+print("program")
